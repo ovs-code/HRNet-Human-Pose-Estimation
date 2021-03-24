cdd13abb7ac2a42ed5bf9b085cc442c6364babc4
Oskar von Seeler <oskar@vonseeler.de>
2021-03-24 14:47:58 +0100

    update inference to always do post-processing

--- hrnet_pose/core/inference.py
+++ hrnet_pose/core/inference.py
@@ -9,10 +9,7 @@ from __future__ import division
 from __future__ import print_function
 
 import math
-
 import numpy as np
-
-from ..utils.transforms import transform_preds
 
 
 def get_max_preds(batch_heatmaps):
@@ -46,27 +43,26 @@ def get_max_preds(batch_heatmaps):
     return preds, maxvals
 
 
-def get_final_preds(config, batch_heatmaps):
-    import math
+def get_final_preds(batch_heatmaps):
     coords, maxvals = get_max_preds(batch_heatmaps)
 
     heatmap_height = batch_heatmaps.shape[2]
     heatmap_width = batch_heatmaps.shape[3]
 
     # post-processing
-    if config.TEST.POST_PROCESS:
-        for n in range(coords.shape[0]):
-            for p in range(coords.shape[1]):
-                hm = batch_heatmaps[n][p]
-                px = int(math.floor(coords[n][p][0] + 0.5))
-                py = int(math.floor(coords[n][p][1] + 0.5))
-                if 1 < px < heatmap_width-1 and 1 < py < heatmap_height-1:
-                    diff = np.array(
-                        [
-                            hm[py][px+1] - hm[py][px-1],
-                            hm[py+1][px]-hm[py-1][px]
-                        ]
-                    )
-                    coords[n][p] += np.sign(diff) * .25
+
+    for n in range(coords.shape[0]):
+        for p in range(coords.shape[1]):
+            hm = batch_heatmaps[n][p]
+            px = int(math.floor(coords[n][p][0] + 0.5))
+            py = int(math.floor(coords[n][p][1] + 0.5))
+            if 1 < px < heatmap_width-1 and 1 < py < heatmap_height-1:
+                diff = np.array(
+                    [
+                        hm[py][px+1] - hm[py][px-1],
+                        hm[py+1][px]-hm[py-1][px]
+                    ]
+                )
+                coords[n][p] += np.sign(diff) * .25
 
     return coords, maxvals
